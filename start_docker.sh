88eacb75f5c38010d4dc4cef0b3a0ab73ff20b34
itechbear <itechbear@gmail.com>
2017-05-16 13:16:05 +0800

    update

--- start_docker.sh
+++ start_docker.sh
@@ -2,8 +2,8 @@
 
 mkidr -p whisper grafana >/dev/null 2>&1
 sudo docker run -d \
-    -p 2003:2003 \
-    -p 3000:3000 \
+    -p 127.0.0.1:2003:2003 \
+    -p 127.0.0.1:3000:3000 \
     -v whisper:/opt/grafana/storage/whisper \
     -v grafana:/var/lib/grafana \
     ubuntu/grafana
